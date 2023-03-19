4327698ddac04b9a085efbf46e516a049ec184a6
tumbleweed <tumbleWeed_06@proton.me>
2023-03-19 19:37:58 +0530

    operation funcs

---
+++ script.js
@@ -0,0 +1,33 @@
+let addition = (num1,num2) => {
+    return num1 + num2;
+}
+
+let subtraction = (num1,num2) => {
+    return num1 - num2;
+}
+
+let multiplication = (num1,num2) => {
+    return num1 * num2;
+}
+
+let division = (num1,num2) => {
+    return num1 / num2;
+}
+
+
+
+
+
+/**
+ * input:1
+ *      :+
+ *      :1
+ * 
+ * 
+ * code:num1 = 1
+ *      plus = true
+ *      num2 = 1
+ * 
+ * 
+ * 
+ */
