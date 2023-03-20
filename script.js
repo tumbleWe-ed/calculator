36ea359a0d24f775f2ebdbc8b141c47c8405c7c9
tumbleweed <tumbleWeed_06@proton.me>
2023-03-20 20:05:27 +0530

    Addition Works

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+//Operations
 let addition = (num1,num2) => {
-    return num1 + num2;
+    return Number(num1) + Number(num2);
 }
 
 let subtraction = (num1,num2) => {
@@ -14,6 +15,8 @@ let division = (num1,num2) => {
     return num1 / num2;
 }
 
+
+// Input Numbers
 let one = document.querySelector('.btn-1')
 let two = document.querySelector('.btn-2')
 let three = document.querySelector('.btn-3')
@@ -25,80 +28,202 @@ let eight = document.querySelector('.btn-8')
 let nine = document.querySelector('.btn-9')
 let zero = document.querySelector('.btn-0')
 
-let btnContainer = document.querySelector('.btn-container')
-let output = document.querySelector('#output')
+let btnNums = Object.values(document.getElementsByClassName('btn-num'))
+
+//let btnContainer = document.querySelector('.btn-container')
+
+//display, clear, and backspace buttons
+let display = document.querySelector('#output')
+display.textContent = '0'
+
 
 let clearBtn = document.querySelector('.btn-clear')
 let backspaceBtn = document.querySelector('.btn-backspace')
 
-let num1 = 0;
 
-btnContainer.addEventListener('click', () => {
-    output.textContent = num1
+
+
+
+
+
+//Operators
+let add = document.querySelector('.btn-add')
+let sutract = document.querySelector('.btn-subtract')
+let multiply = document.querySelector('.btn-multiply')
+let divide = document.querySelector('.btn-divide')
+
+let btnsOnOperatorColumn = Object.values(document.getElementsByClassName('operator-column'))
+
+let num1 = 0;
+let num2 = 0;
+
+let onOperator;
+
+let onNum1 = true;
+let onNum2 = false;
+
+let output = String();
+//Equal button
+let equal = document.querySelector('.btn-equal')
+
+
+//Operations
+
+add.addEventListener('click', () => {
+    output = `${num1} + ${num2}` 
+    display.textContent = '+'
+    onNum2 = true
+    onNum1 = false
+    
+    onOperator = 'add'
+
+    console.log(output)
 })
 
+
+
+
+//Output for Clear Button, Backspace Button
 clearBtn.addEventListener('click', () => {
     num1 = 0;
+    num2 = 0;
+    onNum1 = true
+    output = '0';
+    display.textContent = output
+
+
+    console.log(`Num1: ${num1}`)
+    console.log(`Num2: ${num2}`)
 })
 
-backspaceBtn.addEventListener('click',() => {
-    num1 = num1.slice(0,num1.length -1)
-})
+/**backspaceBtn.addEventListener('click',() => {
+    output = output.slice(0,output.length -1)
+    display.textContent = output
+})**/
 
-
-
-
+//Output for numbers
 one.addEventListener('click',() => {
-    num1 = `${num1}${1}`
+    if(onNum1){
+        num1 = `${num1}${1}`
+    } else if (onNum2) {
+        num2 = `${num2}${1}`
+    }
 })
 
 two.addEventListener('click',() => {
-    num1 = `${num1}${2}`
+    if(onNum1){
+        num1 = `${num1}${2}`
+    } else if (onNum2) {
+        num2 = `${num2}${2}`
+    }
+
 })
 
 three.addEventListener('click',() => {
-    num1 = `${num1}${3}`
+    if(onNum1){
+        num1 = `${num1}${3}`
+    } else if (onNum2) {
+        num2 = `${num2}${3}`
+    }
+
 })
 
 four.addEventListener('click',() => {
-    num1 = `${num1}${4}`
+    if(onNum1){
+        num1 = `${num1}${4}`
+    } else if (onNum2) {
+        num2 = `${num2}${4}`
+    }
 })
 
 five.addEventListener('click',() => {
-    num1 = `${num1}${5}`
+    if(onNum1){
+        num1 = `${num1}${5}`
+    } else if (onNum2) {
+        num2 = `${num2}${5}`
+    }
+
 })
 
 six.addEventListener('click',() => {
-    num1 = `${num1}${6}`
+    if(onNum1){
+        num1 = `${num1}${6}`
+    } else if (onNum2) {
+        num2 = `${num2}${6}`
+    }
+
 })
 
 seven.addEventListener('click',() => {
-    num1 = `${num1}${7}`
+    if(onNum1){
+        num1 = `${num1}${7}`
+    } else if (onNum2) {
+        num2 = `${num2}${7}`
+    }
 })
 
 eight.addEventListener('click',() => {
-    num1 = `${num1}${8}`
+    if(onNum1){
+        num1 = `${num1}${8}`
+    } else if (onNum2) {
+        num2 = `${num2}${8}`
+    }
+
 })
 
 nine.addEventListener('click',() => {
-    num1 = `${num1}${9}`
+    if(onNum1){
+        num1 = `${num1}${9}`
+    } else if (onNum2) {
+        num2 = `${num2}${9}`
+    }
+
 })
 
 zero.addEventListener('click',() => {
-    num1 = `${num1}${0}`
+    if(onNum1){
+        num1 = `${num1}${0}`
+    } else if (onNum2) {
+        num2 = `${num2}${0}`
+    }
 })
 
 
-/**
- * input:1
- *      :+
- *      :1
- * 
- * 
- * code:num1 = 1
- *      plus = true
- *      num2 = 1
- * 
- * 
- * 
- */
+btnNums.forEach(btn => {
+    btn.addEventListener('click',() => {
+       if(onNum1){
+        output = `${num1}`
+        display.textContent = output
+       } else if(onNum2) {
+        output = `${num1} + ${num2}` 
+        display.textContent = output
+       }
+    
+    console.log(`Num1: ${num1}`)
+    console.log(`Num2: ${num2}`)
+    })
+    
+})
+
+btnsOnOperatorColumn.forEach(btn => {
+    btn.addEventListener('click', () => {
+        //output = addition(num1,num2)
+        if(onOperator === 'add'){
+            //display.textContent = '+'
+            output = `${addition(num1,num2)} +`
+        } 
+        equal.addEventListener('click', () => {
+            output = output.slice(0,output.length - 2)
+            display.textContent = output
+        })
+
+        display.textContent = output;
+
+    console.log(`Num1: ${num1}`)
+    console.log(`Num2: ${num2}`)
+    })
+})
+
+console.log(`Num1: ${num1}`)
+console.log(`Num2: ${num2}`)
+console.log(output)
